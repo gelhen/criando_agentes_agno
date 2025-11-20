187d3012f5e14f4aea9177a7c8169f90c2daa3b6
Andre Marcos Gelhen <gelhen@gmail.com>
2025-11-20 16:18:05 -0300

    Utilizando tool própria

---
+++ 1.3.own_tolls.py
@@ -0,0 +1,32 @@
+from sys import flags
+from agno.agent import Agent
+from agno.tools.tavily import TavilyTools
+from agno.models.groq import Groq
+from dotenv import load_dotenv
+load_dotenv()
+
+
+def celsius_to_fh(temperatura_celsius: float):
+    """
+    Converte uma temperatura de Celsius para Fahrenheit.
+
+    Args:
+        temperatura_celsius (float): Temperatura em graus Celsius.
+
+    Returns:
+        float: Temperatura convertida para Fahrenheit.
+    """
+    return (temperatura_celsius * 9/5) + 32
+
+
+
+agent = Agent(
+    model=Groq(id="llama-3.3-70b-versatile"),
+    tools=[
+        TavilyTools(),
+        celsius_to_fh
+        ],
+    debug_mode=True
+)
+
+agent.print_response("Use suas ferramentas para pesquisar a temperatura de hoje em Machadinho RS em Fahrenheit")
