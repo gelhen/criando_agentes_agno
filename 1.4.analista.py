b26722820672cda51b7d62206c033e25d1b82e33
Andre Marcos Gelhen <gelhen@gmail.com>
2025-12-31 17:39:31 -0300

    Revisado e incluido comentários no 1.4.analista

--- 1.4.analista.py
+++ 1.4.analista.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# Setup the SQLite database
+# Setup the SQLite database para criar um storage
 db = SqliteDb(db_file="tmp/data.db")
 
 agent = Agent(
@@ -16,11 +16,14 @@ agent = Agent(
     model=OpenAIChat(id="gpt-5-nano", api_key=os.getenv("OPENAI_API_KEY")),
     tools=[YFinanceTools()],
     instructions="Use tabelas para mostrar a informação final. Não inclua nenhum outro texto.",
-    db=db,
-    add_history_to_context=True,
+    db=db, # base de dados para armazenar a sessão, resumindo, não é memória para o agente mas é local para recuperar informações passadas, aqui só um histórico de conversas
+    add_history_to_context=True, # adiciona historico de conversas
     num_history_runs=3 # numero de conversas que o agente vai lembrar   
 )
 
+#informando session_id e user_id por conversa o agente vai registrar id conforme passado
+# quando passado diretamente pelo agente registra session_id aleatoria
+#outro detalhe, o registro é pro sesseion_id, não inporta qual usuario_id está perguntando
 agent.print_response("Qual é a cotação da petrobras?", session_id="petrobras_session", user_id="analista_petrobras")
 agent.print_response("Qual é a cotação da vale?", session_id="vale_session", user_id="analista_vale")
 agent.print_response("Quais empresas ja consultamos a cotação?", session_id="petrobras_session", user_id="analista_empresas")
